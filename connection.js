791792f57f69338f29498a75c1db6d34beb86cae
Wilson Jonathan Oey <61810752+wilsonoey60@users.noreply.github.com>
2024-02-03 15:23:56 +0800

    Update connection.js

--- connection.js
+++ connection.js
@@ -7,7 +7,6 @@ const connection = createConnection({
   database: process.env.NAMEDB,
 });
 connection.connect((checker) => console.log(checker));
-// tampilkan di terminal setiap tindakan database menggunakan method yang ada
 connection.on('enqueue', (sequence) => (sequence.constructor.name === 'Query' ? console.log(sequence.sql) : null));
 
 module.exports = connection;
