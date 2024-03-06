98ccf5b917d0e6514af30a4cfe2ca21170709c77
Wilson Jonathan Oey <61810752+wilsonoey60@users.noreply.github.com>
2024-03-06 20:03:38 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const routes = require('./routes');
 const { success } = require('./response');
 const connection = require('./connection');
 const { nanoid } = require('nanoid');
+const swagger = require('hapi-swagger');
+const Inert = require('@hapi/inert');
+const Vision = require('@hapi/vision');
 
 const init = async () => {
   const server = Hapi.server({
@@ -37,6 +40,32 @@ const init = async () => {
     // jika bukan error, lanjutkan dengan response sebelumnya (tanpa terintervensi)
     return h.continue;
   });
+  
+  await server.register([
+    Inert,
+    Vision,
+    {
+      plugin: swagger,
+      options: {
+        info: {
+          title: 'bookapp-public API Documentation',
+          version: '1.0.11',
+        },
+        tags : [
+          {
+            name: 'books',
+            description: 'Documentations for books'
+          },
+          {
+            name: 'errors',
+            description: 'Menampilkan error yang ada selama proses request'
+          },
+        ],
+        documentationPath: '/',
+        grouping: 'tags',
+      },
+    }
+  ]);
   
   server.route(routes);
   await server.start();
