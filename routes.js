801ab5b293a9e72d3516193c9d3130f3cd7a1e17
Wilson Jonathan Oey <61810752+wilsonoey60@users.noreply.github.com>
2024-04-23 19:14:53 +0800

    Update routes.js

--- routes.js
+++ routes.js
@@ -223,6 +223,11 @@ const routes = [
           status: Joi.string().example('success'),
           message: Joi.string().example('Data has been deleted'),
         }).label('Result').description('Successful response in English'),
+        schema: Joi.object({
+          statusCode: Joi.number().example(404),
+          status: Joi.string().example('fail'),
+          message: Joi.string().example('Data not found'),
+        }).label('Result').description('Failed response in English'),
       },
     },
   },
