0db90dbf036c33ae7fc6d0edcd27d13c619b3585
Wilson Jonathan Oey <61810752+wilsonoey60@users.noreply.github.com>
2024-06-01 17:43:07 +0800

    Implementation mysql2 in connection.js

--- connection.js
+++ connection.js
@@ -1,12 +1,12 @@
-const { createConnection } = require('mysql');
+const mysql2 = require('mysql2/promise');
 
-const connection = createConnection({
+const connection = mysql2.createPool({
   host: process.env.HOSTDB,
   user: process.env.USERDB,
   password: process.env.PASSDB,
   database: process.env.NAMEDB,
 });
-connection.connect((checker) => console.log(checker));
+
 connection.on('enqueue', (sequence) => (sequence.constructor.name === 'Query' ? console.log(sequence.sql) : null));
 
 module.exports = connection;
