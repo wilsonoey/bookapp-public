5537b6d07f951122058b0ee359ed55d0ab7a24ab
Wilson Jonathan Oey <61810752+wilsonoey@users.noreply.github.com>
2024-07-11 19:05:22 +0800

    Update handler.js

--- handler.js
+++ handler.js
@@ -130,10 +130,10 @@ async function deleteBook(request, h) {
 }
 
 function routesOthers(req, res) {
-  console.log(res);
   return res.response({
     status: 'fail',
     message: 'The route you are accessing was not found',
+    data: res
   }).code(404);
 }
 
